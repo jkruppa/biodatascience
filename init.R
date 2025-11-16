77660f7827cb0ebb8e44bdbbf4bdfe9b210292d0
Jochen Kruppa <jkruppa@users.noreply.github.com>
2025-11-16 21:01:17 +0100

    added

---
+++ init.R
@@ -0,0 +1,15 @@
+## load packages
+pacman::p_load(tidyverse, ggforce, viridis, knitr, patchwork)
+## ggplot theme
+theme_book <- function() {
+  theme_minimal() +
+    theme(panel.grid.minor = element_blank(),
+          plot.title = element_text(size = 16, face = "bold"),
+          plot.subtitle = element_text(size = 12, face = "italic"),
+          plot.caption = element_text(face = "italic"),
+          axis.title = element_text(size = 12, face = "bold"),
+          axis.text = element_text(size = 11),
+          legend.title = element_text(face = "bold"))
+}
+## colors
+col_pal <- \(n, alpha) plasma(n = n, alpha = alpha)
