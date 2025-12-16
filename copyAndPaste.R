45ffa680f04ea6b4ad35cea9b11162206adc1649
Jochen Kruppa <jkruppa@users.noreply.github.com>
2025-12-16 16:25:10 +0100

    added

--- copyAndPaste.R
+++ copyAndPaste.R
@@ -1,87 +1,5 @@
-library(tidyverse)
 
-geom_coord_cross <- function(x, y, yticks = "numeric", xticks = "numeric", zero = FALSE) {
-  list(
-    annotate("segment", x = x-0.2, xend = x+4, y = y, yend = y,
-             arrow = arrow(length = unit(0.0075, "npc"), type = "closed")),
-    annotate("segment", x = x, xend = x, y = y-0.2, yend = y+4,
-             arrow = arrow(length = unit(0.0075, "npc"), type = "closed")),
-    if(yticks == "numeric") {
-      if(zero){
-        list(
-          annotate("segment", x = x-0.1, xend = x, y = y + seq(0, 3.5, 0.5), yend = y+ seq(0, 3.5, 0.5)),
-          annotate("text", x = x -0.25, y = y + seq(0, 3.5, 0.5), label = seq(0, 70, 10),
-                   hjust = "right"),
-          annotate("text", x = x - 1, y = y +2, label = c("Count of hairleg"), fontface = 2, size = 5,
-                   angle = 90)
-        )
-      } else {
-        list(
-          annotate("segment", x = x-0.1, xend = x, y = y + seq(0, 3.5, 0.5), yend = y+ seq(0, 3.5, 0.5)),
-          annotate("text", x = x -0.25, y = y + seq(0.5, 3.5, 0.5), 
-                   label = round(seq(15, 40, length.out = 7)), hjust = "right") ,
-          annotate("text", x = x - 1, y = y +2, label = c("Jump length"), fontface = 2, size = 5, 
-                   angle = 90)
-        )        
-      }
-    },
-    if(xticks == "numeric") {
-      list(
-        annotate("segment", x = x+ seq(0, 3.5, 0.5), xend = x+ seq(0, 3.5, 0.5), y = y -0.1 , yend = y),
-        annotate("text", x = x +seq(0.5, 3.5, 0.5), y = y -0.35, label = seq(0.5, 3.5, 0.5)),
-        annotate("text", x = x + 2, y = y -1, label = c("Weight"), fontface = 2, size = 5)
-      )
-    },
-    if(yticks == "3lvl") {
-      list(
-        annotate("segment", x = x-0.1, xend = x, y = y + c(1,2,3), yend = y+ c(1,2,3)),
-        annotate("text", x = x -0.25, y = y + c(1,2,3), label = c("1", "2", "3"), hjust = "right") ,
-        annotate("text", x = x - 1, y = y +2, label = c("Grade"), fontface = 2, size = 5, angle = 90)
-      )
-    },
-    if(xticks == "3lvl") {
-      list(
-        annotate("segment", x = x + c(1,2,3), xend = x+ c(1,2,3), y = y -0.1 , yend = y),
-        annotate("text", x = x + c(1,2,3), y = y -0.35, label = c("dog", "cat", "fox")),
-        annotate("text", x = x + 2, y = y -1, label = c("Host"), fontface = 2, size = 5)
-      )
-    },
-    if(yticks == "2lvl") {
-      list(
-        annotate("segment", x = x-0.1, xend = x, y = y + c(0.5, 3), yend = y+  c(0.5, 3)),
-        annotate("text", x = x -0.25, y = y + c(0.5, 3), label = c("0", "1"), hjust = "right") ,
-        annotate("text", x = x - 1, y = y +2, label = c("Infected"), fontface = 2, size = 5, angle = 90)
-      )
-    },
-    if(xticks == "2lvl") {
-      list(
-        annotate("segment", x = x+ c(1.25, 2.75), xend = x+ c(1.25, 2.75), y = y -0.1 , yend = y),
-        annotate("text", x = x + c(1.25, 2.75), y = y -0.35, label = c("dog", "cat")),
-        annotate("text", x = x + 2, y = y -1, label = c("Host"), fontface = 2, size = 5)
-        
-      )
-    }
-  )
-}
-
-ggplot() +
-  ## theme_void() +
-  theme_minimal() +
-  coord_cartesian(xlim = c(-1, 20), ylim = c(0, 27)) +
-  scale_x_continuous(breaks = seq(-10, 10, 1)) +
-  scale_y_continuous(breaks = seq(0, 10, 1)) +
-  annotate("segment", x = c(2, 8, 14, 20), xend = c(2, 8, 14, 20),
-           y = c(0, 0, 0, 0), yend = c(27, 26, 26, 27)) +
-  annotate("segment", x = c(-1, 0, 0, 0, -1), xend = c(20, 20, 20, 20, 20),
-           y = c(0, 6, 12, 18, 24), yend = c(0, 6, 12, 18, 24)) +
-  geom_segment(y = c(0, 6, 12, 18, 24)) +
-  scale_x_continuous(breaks = 0:22, expand = expansion(mult = 0)) +
-  scale_y_continuous(breaks = 0:28, expand = expansion(mult = 0)) +
-  ## outer text
-  annotate("text", x = 11, y = 26.5, label = "Influencer (X)", size = 8, fontface = 2) +
-  annotate("text", x = -0.5, y = 12, label = "Outcome (Y)", size = 8, fontface = 2, angle = 90) +
-  ## coords
-  geom_coord_cross(x = 3.5, y = 19.5, yticks = "numeric", xticks = "numeric") +
+geom_coord_cross(x = 3.5, y = 19.5, yticks = "numeric", xticks = "numeric") +
   geom_coord_cross(x = 9.5, y = 19.5, yticks = "numeric", xticks = "3lvl") +
   geom_coord_cross(x = 15.5, y = 19.5, yticks = "numeric", xticks = "2lvl") +
   geom_coord_cross(x = 3.5, y = 13.5, yticks = "numeric", xticks = "numeric", zero = TRUE) +
@@ -93,12 +11,12 @@ ggplot() +
   geom_coord_cross(x = 3.5, y = 1.5, yticks = "2lvl", xticks = "numeric") +
   geom_coord_cross(x = 9.5, y = 1.5, yticks = "2lvl", xticks = "3lvl") +
   geom_coord_cross(x = 15.5, y = 1.5, yticks = "2lvl", xticks = "2lvl") +
-  ## normal
-  ### covariate
-  annotate("point", x = c(0.5, 1, 1.25, 1.7, 2.5, 3.2) + 3.5, 
-           y = c(20.5, 21, 21.5, 22, 22.5, 22)) +
+
+### covariate
+annotate("point", x = c(0.5, 1, 1.25, 1.7, 2.5, 3.2) + 3.5, 
+         y = c(20.5, 21, 21.5, 22, 22.5, 22), size = 1.5) +
   annotate("segment", x = 3.75, xend = 7.25, y = 20.5, yend = 23, linewidth = 0.65) +
-  geom_curve(aes(x = 4, y = 20, xend = 7.25, yend = 21.5),
+  geom_curve(aes(x = 4, y = 20, xend = 7.25, yend = 22.5),
              curvature = -0.4, color = "black", linewidth = 0.65) +
   ### 3lvl
   geom_rect(aes(xmin = 10.1, xmax = 10.35, ymin = 19.5, ymax = 22.25), fill = "#B12A9080",
@@ -106,7 +24,7 @@ ggplot() +
   geom_segment(aes(x = 10.225, xend = 10.225, y = 22.25, yend = 22.75)) +
   geom_segment(aes(x = 10.15, xend = 10.3, y = 22.75, yend = 22.75)) +  
   annotate("point", x = c(10.5), 
-           y = c(21.5, 22, 22.5, 22.75, 23)) +  
+           y = c(21.5, 22, 22.5, 22.75, 23), size = 1.5) +  
   geom_rect(aes(xmin = 10.65, xmax = 10.9, ymin = 21.5, ymax = 23), fill = "#FCA63680",
             color = "black") +
   geom_segment(aes(x = 10.65, xend = 10.9, y = 22.5, yend = 22.5)) +
@@ -116,7 +34,7 @@ ggplot() +
   geom_segment(aes(x = 11.225, xend = 11.225, y = 20.5, yend = 21)) +
   geom_segment(aes(x = 11.15, xend = 11.3, y = 21, yend = 21)) +  
   annotate("point", x = c(11.5), 
-           y = c(19.75, 20, 20.5, 21, 21.25)) +  
+           y = c(19.75, 20, 20.5, 21, 21.25), size = 1.25) +  
   geom_rect(aes(xmin = 11.65, xmax = 11.9, ymin = 20, ymax = 21), fill = "#FCA63680",
             color = "black") +
   geom_segment(aes(x = 11.65, xend = 11.9, y = 20.5, yend = 20.5)) +
@@ -126,7 +44,7 @@ ggplot() +
   geom_segment(aes(x = 12.225, xend = 12.225, y = 21.75, yend = 22.25)) +
   geom_segment(aes(x = 12.15, xend = 12.3, y = 22.25, yend = 22.25)) +    
   annotate("point", x = c(12.5), 
-           y = c(20.75, 21.25, 21.5, 22.2, 22.5)) +
+           y = c(20.75, 21.25, 21.5, 22.2, 22.5), size = 1.25) +
   geom_rect(aes(xmin = 12.65, xmax = 12.9, ymin = 21, ymax = 22.25), fill = "#FCA63680",
             color = "black") +
   geom_segment(aes(x = 12.65, xend = 12.9, y = 21.5, yend = 21.5)) +
@@ -137,7 +55,7 @@ ggplot() +
   geom_segment(aes(x = 10.225+6.25, xend = 10.225+6.25, y = 22.25, yend = 22.75)) +
   geom_segment(aes(x = 10.15+6.25, xend = 10.3+6.25, y = 22.75, yend = 22.75)) +  
   annotate("point", x = c(10.5)+6.25, 
-           y = c(21.5, 22, 22.5, 22.75, 23)) +  
+           y = c(21.5, 22, 22.5, 22.75, 23), size = 1.25) +  
   geom_rect(aes(xmin = 10.65+6.25, xmax = 10.9+6.25, ymin = 21.5, ymax = 23), fill = "#FCA63680",
             color = "black") +
   geom_segment(aes(x = 10.65+6.25, xend = 10.9+6.25, y = 22.5, yend = 22.5)) +
@@ -147,28 +65,11 @@ ggplot() +
   geom_segment(aes(x = 11.225+6.75, xend = 11.225+6.75, y = 20.5, yend = 21)) +
   geom_segment(aes(x = 11.15+6.75, xend = 11.3+6.75, y = 21, yend = 21)) +  
   annotate("point", x = c(11.5+6.75), 
-           y = c(19.75, 20, 20.5, 21, 21.25)) +  
+           y = c(19.75, 20, 20.5, 21, 21.25), size = 1.25) +  
   geom_rect(aes(xmin = 11.65+6.75, xmax = 11.9+6.75, ymin = 20, ymax = 21), fill = "#FCA63680",
             color = "black") +
   geom_segment(aes(x = 11.65+6.75, xend = 11.9+6.75, y = 20.5, yend = 20.5)) +
-  geom_segment(aes(x = 11.775+6.75, xend = 11.775+6.75, y = c(19.7, 21), yend = c(20, 21.3))) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  geom_segment(aes(x = 11.775+6.75, xend = 11.775+6.75, y = c(19.7, 21), yend = c(20, 21.3))) +
 
 
 
