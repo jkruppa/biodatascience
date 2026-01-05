a41cce1814570d0f1e5c71e850247d6aa5974663
Jochen Kruppa <jkruppa@users.noreply.github.com>
2026-01-05 12:46:07 +0100

    added

--- init.R
+++ init.R
@@ -1,6 +1,7 @@
 ## load packages
 pacman::p_load(tidyverse, ggforce, viridis, knitr, patchwork, lme4,
-               gghalves, ggdist, see, ggtext, ggimage, ggrepel,
+               #gghalves, 
+               ggdist, see, ggtext, ggimage, ggrepel,
                conflicted)
 conflicted::conflicts_prefer(dplyr::filter)
 ## global seed
